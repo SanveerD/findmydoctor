6c62df9f7fa685fd7c33f9cb68e9f8f1a827d86b
sanveer <dhanju.sanveer@gmail.com>
2024-10-30 18:12:21 -0400

    init

---
+++ splash-page/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="page-content">
+      <h2>About Us</h2>
+      <p>We are a company dedicated to creating innovative solutions for our customers. Our app is currently under development and will be available soon.</p>
+      <p>Our team of experts is working tirelessly to bring you a product that will revolutionize the industry.</p>
+    </div>
+  );
+}
+
+export default About;
